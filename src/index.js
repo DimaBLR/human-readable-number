04c65fcdb5c75777dfe242ec053186a8c202fd26
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-19 14:38:52 +0300

    docs: update code (short)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
     if (!number) return 'zero';
-    let result;
-    let ArrNum = String(number).split('');
+    let result, n = String(number), s = +(n[1] + n[2]);
     let num = ['','one','two','three','four','five','six','seven','eight','nine','ten',
             'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen',
             'eighteen','nineteen'];
     let num10 = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-
-    let i0 = ArrNum[0]; let i1 = ArrNum[1]; let i2 = ArrNum[2];
-    let i3 = +(ArrNum[1] + ArrNum[2]);
-
-    if (number < 20) {
-        result = num[number];
-    } else if (number < 100) {
-        result = num10[i0] + ' ' + num[i1];
-    } else if (number > 99 && i3 <= 19) {
-        result = num[i0] + ' ' + num10[1] + ' ' + num[i3];
-    } else if (number > 99 && i3 > 19) {
-        result = num[i0] + ' ' + num10[1] + ' ' + num10[i1] + ' ' + num[i2];
-    }	
+		if (number < 20) {
+			result = num[number];
+		} else if (number < 100) {
+			result = num10[n[0]] + ' ' + num[n[1]];
+		} else if (number > 99 && s <= 19) {
+			result = num[n[0]] + ' ' + num10[1] + ' ' + num[s];
+		} else if (number > 99 && s > 19) {
+			result = num[n[0]] + ' ' + num10[1] + ' ' + num10[n[1]] + ' ' + num[n[2]];
+		}	
     return result.trim();    
 }
