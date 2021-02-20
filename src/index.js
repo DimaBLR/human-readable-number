959d3d94254004f5ded07dece0f6d381fa9410b5
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-20 11:20:42 +0300

    docs: update code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ module.exports = function toReadable(number) {
     if (!number) return 'zero';
     let result, n = String(number), s = +(n[1] + n[2]);
     let num = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven',
-               'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+        'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let num10 = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
-    if (number < 20) {
-        result = num[number];
-    } else if (number < 100) {
+    if (n < 20) {
+        result = num[n];
+    } else if (n < 100) {
         result = num10[n[0]] + ' ' + num[n[1]];
-    } else if (number > 99 && s <= 19) {
+    } else if (n > 99 && s <= 19) {
         result = num[n[0]] + ' ' + num10[1] + ' ' + num[s];
-    } else if (number > 99 && s > 19) {
+    } else if (n > 99 && s > 19) {
         result = num[n[0]] + ' ' + num10[1] + ' ' + num10[n[1]] + ' ' + num[n[2]];
     }
     return result.trim();
