5b46818f506044b5b5800134b71b3231a93bce5c
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-20 13:18:45 +0300

    docs: removed unused variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable(number) {
     if (!number) return 'zero';
-    let result, n = String(number), s = +(n[1] + n[2]);
+    let n = String(number), s = +(n[1] + n[2]);
     let num = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven',
         'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let ten = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
