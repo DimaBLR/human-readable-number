65c4e544178579bcb16818f5a940f9dd669da034
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-20 11:31:40 +0300

    docs: update code

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ module.exports = function toReadable(number) {
         'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let ten = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     if (n < 20) {
-        result = num[n];
+        return num[n];
     } else if (n < 100) {
-        result = ten[n[0]] + ' ' + num[n[1]];
+        return (ten[n[0]] + ' ' + num[n[1]]).trim();
     } else if (n > 99 && s <= 19) {
-        result = num[n[0]] + ' ' + ten[1] + ' ' + num[s];
+        return (num[n[0]] + ' ' + ten[1] + ' ' + num[s]).trim();
     } else if (n > 99 && s > 19) {
-        result = num[n[0]] + ' ' + ten[1] + ' ' + ten[n[1]] + ' ' + num[n[2]];
-    }
-    return result.trim();
+        return (num[n[0]] + ' ' + ten[1] + ' ' + ten[n[1]] + ' ' + num[n[2]]).trim();
+    }    
 }
