88cf21bb40ef324822949a3da6e6be0d258e5ab2
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-20 11:25:43 +0300

    docs: update name ten

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ module.exports = function toReadable(number) {
     let result, n = String(number), s = +(n[1] + n[2]);
     let num = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven',
         'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-    let num10 = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    let ten = ['','hundred','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
     if (n < 20) {
         result = num[n];
     } else if (n < 100) {
-        result = num10[n[0]] + ' ' + num[n[1]];
+        result = ten[n[0]] + ' ' + num[n[1]];
     } else if (n > 99 && s <= 19) {
-        result = num[n[0]] + ' ' + num10[1] + ' ' + num[s];
+        result = num[n[0]] + ' ' + ten[1] + ' ' + num[s];
     } else if (n > 99 && s > 19) {
-        result = num[n[0]] + ' ' + num10[1] + ' ' + num10[n[1]] + ' ' + num[n[2]];
+        result = num[n[0]] + ' ' + ten[1] + ' ' + ten[n[1]] + ' ' + num[n[2]];
     }
     return result.trim();
 }
